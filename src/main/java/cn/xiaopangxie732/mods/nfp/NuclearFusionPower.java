63eb1da730ea75debdc63fcfefaf23061d799646
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2021-05-04 00:41:23 +0800

    Initial mod base

---
+++ src/main/java/cn/xiaopangxie732/mods/nfp/NuclearFusionPower.java
@@ -0,0 +1,13 @@
+package cn.xiaopangxie732.mods.nfp;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("nfp")
+public class NuclearFusionPower {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public NuclearFusionPower() {
+    }
+}
